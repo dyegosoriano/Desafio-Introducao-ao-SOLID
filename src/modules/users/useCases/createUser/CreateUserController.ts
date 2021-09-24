eee6f011965578f3bc62c7b867b4bb45717d80d0
Dyego Soriano <dyegodf2@gmail.com>
2021-09-24 18:42:36 -0300

    fix: CreateUserController and ListAllUsersController

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -5,12 +5,16 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    const { email, name } = request.body;
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { email, name } = request.body;
 
-    const user = this.createUserUseCase.execute({ email, name });
+      const user = this.createUserUseCase.execute({ email, name });
 
-    return response.status(201).json(user);
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
