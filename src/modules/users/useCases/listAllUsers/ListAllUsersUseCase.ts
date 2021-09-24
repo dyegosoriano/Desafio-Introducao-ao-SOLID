b1004035b87ae83f52de2f29006545c57f09a3a7
Dyego Soriano <dyegodf2@gmail.com>
2021-09-23 21:02:18 -0300

    fix: ListAllUsersUseCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById(user_id);
+    if (!userIsAdmin.admin) {
+      throw new Error("User does not exist or does not have permission");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
